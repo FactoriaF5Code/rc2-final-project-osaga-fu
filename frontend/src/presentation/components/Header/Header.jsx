cac100f860ce160d1be581ce241fa586e5e007df
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-01 11:39:43 +0200

    Header completed

--- frontend/src/presentation/components/Header/Header.jsx
+++ frontend/src/presentation/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import "./Header.css"
+import Logo from "../../assets/images/logo.png"
 
 export default function Header() {
 
   return (
-    <header>Header</header>
+    <header>
+      <img className="logo" src={Logo} alt="La Llama Laser Studio" />
+      <span className="text-container">
+        <h1 className="title-text">La Llama</h1>
+        <h2 className="subtitle-text">Laser Studio</h2>
+      </span>
+    </header>
   )
 }
