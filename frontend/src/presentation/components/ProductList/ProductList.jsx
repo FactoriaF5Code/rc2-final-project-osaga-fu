427a684c8ac3375156407cfc774f54fb29289864
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-03 19:09:37 +0200

    Product list css created

--- frontend/src/presentation/components/ProductList/ProductList.jsx
+++ frontend/src/presentation/components/ProductList/ProductList.jsx
@@ -1,8 +1,21 @@
-import "./ProductList.css"
+import "./ProductList.css";
+import Image from "../../assets/images/_DSC0148-1.jpg";
 
 export default function ProductList() {
-
   return (
-    <div>ProductList</div>
-  )
+    <section className="list-container">
+      <h3 className="list-title">Products</h3>
+      <section className="products-wrapper">
+        <article className="product-container">
+          <div className="product-image-container">
+            <img className="product-image" src={Image} alt="" />
+          </div>
+          <span className="product-info">
+            <h5 className="info-title">Focus Star</h5>
+            <h4 className="info-price">10€</h4>
+          </span>
+        </article>
+      </section>
+    </section>
+  );
 }
