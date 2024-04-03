1c77db83479e860bb32e98ce34de81bf8cdd9c81
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-03 19:50:27 +0200

    Completed product detail component

--- frontend/src/presentation/components/ProductDetail/ProductDetail.jsx
+++ frontend/src/presentation/components/ProductDetail/ProductDetail.jsx
@@ -1,8 +1,24 @@
-import "./ProductDetail.css"
+import "./ProductDetail.css";
+import Image from "../../assets/images/_DSC0148-1.jpg";
 
 export default function ProductDetail() {
-
   return (
-    <div>ProductDetail</div>
-  )
+    <main>
+      <section className="detail-container">
+        <div className="photo-container">
+          <img className="detail-photo" src={Image} alt="" />
+        </div>
+        <aside className="detail-info">
+          <span>
+            <h3 className="text-title">Focus Star</h3>
+            <p className="text-description">
+              Custom your own focus star so you'll never lose it again.
+            </p>
+            <h4 className="detail-price">10€</h4>
+          </span>
+          <button className="back-button">Back</button>
+        </aside>
+      </section>
+    </main>
+  );
 }
