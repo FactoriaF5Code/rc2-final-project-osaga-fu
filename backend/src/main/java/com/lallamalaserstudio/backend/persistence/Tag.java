bdc61f1ef91ab31a407fd4aada2fc2ecf507778e
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-04 17:42:46 +0200

    Tag class implemented

--- backend/src/main/java/com/lallamalaserstudio/backend/persistence/Tag.java
+++ backend/src/main/java/com/lallamalaserstudio/backend/persistence/Tag.java
@@ -1,5 +1,63 @@
 package com.lallamalaserstudio.backend.persistence;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tags")
 public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String size;
+    
+    public Tag(Long id, String name, Double price, String size){
+        this.id = id;
+        this.name=name;
+        this.price=price;
+        this.size=size;
+    }
+
+    public Tag() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
     
 }
