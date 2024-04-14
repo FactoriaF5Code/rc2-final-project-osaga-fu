648f18e55f1841035339f0fa6c091e090f315403
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-14 12:49:16 +0200

    Created CartResponse

--- backend/src/main/java/com/lallamalaserstudio/backend/controllers/cart/CartResponse.java
+++ backend/src/main/java/com/lallamalaserstudio/backend/controllers/cart/CartResponse.java
@@ -1,4 +1,49 @@
 package com.lallamalaserstudio.backend.controllers.cart;
 
 public class CartResponse {
+
+    private Long id;
+    private String text;
+    private String color;
+    private String typography;
+
+    public CartResponse(Long id, String text, String color, String typography) {
+        this.id = id;
+        this.text = text;
+        this.color = color;
+        this.typography = typography;
+    }
+
+
+    public String getTypography() {
+        return typography;
+    }
+
+    public void setTypography(String typography) {
+        this.typography = typography;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
