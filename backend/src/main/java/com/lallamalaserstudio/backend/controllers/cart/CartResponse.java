9925c67849c767e933e7177d372c761ede3d5337
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-15 19:34:34 +0200

    Changed CartResponse

--- backend/src/main/java/com/lallamalaserstudio/backend/controllers/cart/CartResponse.java
+++ backend/src/main/java/com/lallamalaserstudio/backend/controllers/cart/CartResponse.java
@@ -1,6 +1,7 @@
 package com.lallamalaserstudio.backend.controllers.cart;
 
 import com.lallamalaserstudio.backend.persistence.cart.Cart;
+import com.lallamalaserstudio.backend.persistence.tag.Tag;
 
 public class CartResponse {
 
@@ -9,18 +10,22 @@ public class CartResponse {
     private String text;
     private String color;
     private String typography;
-    private Long tagId;
     private String tagName;
+    private Long tagId;
+    private Double tagPrice;
+    private String photoUrl;
 
 
-    public CartResponse(Long id, Integer quantity, String text, String color, String typography, Long tagId, String tagName) {
+    public CartResponse(Long id, Integer quantity, String text, String color, String typography, String tagName, Long tagId, Double tagPrice, String photoUrl) {
         this.id = id;
         this.quantity = quantity;
         this.text = text;
         this.color = color;
         this.typography = typography;
-        this.tagId = tagId;
         this.tagName = tagName;
+        this.tagId = tagId;
+        this.tagPrice = tagPrice;
+        this.photoUrl = photoUrl;
     }
 
 
@@ -65,7 +70,15 @@ public class CartResponse {
     }
 
     public static CartResponse fromCart(Cart cart) {
-        return new CartResponse(cart.getId(), cart.getQuantity(), cart.getText(), cart.getColor(), cart.getTypography(), cart.getTag().getId(), cart.getTag().getName());
+        return new CartResponse(cart.getId(), cart.getQuantity(), cart.getText(), cart.getColor(), cart.getTypography(), cart.getTag().getName(), cart.getTag().getId(), cart.getTag().getPrice(), cart.getTag().getPhotoUrl());
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
     }
 
     public Long getTagId() {
@@ -76,11 +89,19 @@ public class CartResponse {
         this.tagId = tagId;
     }
 
-    public String getTagName() {
-        return tagName;
+    public Double getTagPrice() {
+        return tagPrice;
     }
 
-    public void setTagName(String tagName) {
-        this.tagName = tagName;
+    public void setTagPrice(Double tagPrice) {
+        this.tagPrice = tagPrice;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
     }
 }
