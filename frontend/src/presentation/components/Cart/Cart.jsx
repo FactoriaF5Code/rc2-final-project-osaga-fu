2f4a406f4c764daf386ddab9ef24d3edb11ddc08
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-15 20:05:17 +0200

    Created delete function on cart component

--- frontend/src/presentation/components/Cart/Cart.jsx
+++ frontend/src/presentation/components/Cart/Cart.jsx
@@ -1,10 +1,14 @@
 import "./Cart.css";
-import picture from "../../assets/images/_DSC0148-1.jpg";
 import { useCart } from "../../../middleware/context/CartContext";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
-  const { cart, fetchCart } = useCart();
+  const { cart, fetchCart, deleteCart } = useCart();
+
+  const handleDelete = (cartId) => {
+    deleteCart(cartId);
+  };
 
   useEffect(() => {
     fetchCart();
@@ -15,12 +19,20 @@ export default function Cart() {
       <section className="list-container">
         <h3 className="list-title">Cart</h3>
         <section className="cart-wrapper">
-          {cart.length == 0 && <h4 className="no-elements">No hay elementos en la cesta</h4>}
+          {cart.length == 0 && (
+            <h4 className="no-elements">No hay elementos en la cesta</h4>
+          )}
           {cart.map((cart) => (
             <article key={cart.id}>
-              <div className="cart-image-container">
-                <img className="cart-image" src={cart.photoUrl} alt={cart.tagName} />
-              </div>
+              <Link to={`/products/${cart.tagId}`}>
+                <div className="cart-image-container">
+                  <img
+                    className="cart-image"
+                    src={cart.photoUrl}
+                    alt={cart.tagName}
+                  />
+                </div>
+              </Link>
               <span className="cart-text">
                 <h5>{cart.tagName}</h5>
                 <h6>{cart.text}</h6>
@@ -35,7 +47,12 @@ export default function Cart() {
               </span>
               <div className="right-section-card">
                 <button className="modify-button">Modify</button>
-                <button className="delete-button">Delete</button>
+                <button
+                  className="delete-button"
+                  onClick={() => handleDelete(cart.id)}
+                >
+                  Delete
+                </button>
               </div>
             </article>
           ))}
