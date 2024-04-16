6c17df3bfac7dd00c69b1c63c149b25b004bbca1
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-16 10:01:45 +0200

    AddCart test created

--- backend/src/test/java/com/lallamalaserstudio/backend/CartControllerTest.java
+++ backend/src/test/java/com/lallamalaserstudio/backend/CartControllerTest.java
@@ -1,5 +1,7 @@
 package com.lallamalaserstudio.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lallamalaserstudio.backend.controllers.cart.CartRequest;
 import com.lallamalaserstudio.backend.controllers.cart.CartResponse;
 import com.lallamalaserstudio.backend.services.CartService;
 import org.junit.jupiter.api.Test;
@@ -7,13 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -22,6 +28,9 @@ public class CartControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private CartService cartService;
@@ -55,5 +64,30 @@ public class CartControllerTest {
                 .andExpect(jsonPath("$[1].tagPrice").value(1.8))
                 .andExpect(jsonPath("$[1].photoUrl").value("photo2"));
 
+    }
+
+    @Test
+    public void testAddCart() throws Exception {
+
+        CartRequest cartRequest = new CartRequest("Texto", "Green", "Roboto", 1, 1L);
+
+        CartResponse cartResponse = new CartResponse(1L, 1, "Texto", "Green", "Roboto", "Focus Star", 1L, 7.0, "photo");
+        when(cartService.addCart(any(CartRequest.class))).thenReturn(cartResponse);
+
+        String jsonRequest = objectMapper.writeValueAsString(cartRequest);
+
+        mvc.perform(post("/api/cart")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.text").value("Texto"))
+                .andExpect(jsonPath("$.color").value("Green"))
+                .andExpect(jsonPath("$.typography").value("Roboto"))
+                .andExpect(jsonPath("$.tagName").value("Focus Star"))
+                .andExpect(jsonPath("$.tagId").value(1))
+                .andExpect(jsonPath("$.tagPrice").value(7.0))
+                .andExpect(jsonPath("$.photoUrl").value("photo"));
     }
 }
