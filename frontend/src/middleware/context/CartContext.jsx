edd1104d349c31e8b7af30b3c7fb8f7482294d31
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-17 14:54:59 +0200

    Deleted unused functions on context

--- frontend/src/middleware/context/CartContext.jsx
+++ frontend/src/middleware/context/CartContext.jsx
@@ -5,7 +5,6 @@ const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
-  const [showModal, setShowModal] = useState(false);
 
   const cartService = new CartService();
 
